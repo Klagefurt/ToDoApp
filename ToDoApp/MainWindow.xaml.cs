c8f8ba9ac46d9803e47bb1716f1c60184c1d13d4
Nikolay Pomogaev <klagefurt@gmail.com>
2024-05-31 13:07:41 +0300

    Add project files.

---
+++ ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoApp.Models;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<ToDoModel> _toDoList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _toDoList = new BindingList<ToDoModel>()
+            {
+                new ToDoModel(){Text = "First Note"},
+                new ToDoModel(){Text = "Second Note"},
+                new ToDoModel(){Text = "New Note..."}
+            };
+
+            DataGridToDoList.ItemsSource = _toDoList;
+            _toDoList.ListChanged += _toDoList_ListChanged;
+        }
+
+        private void _toDoList_ListChanged(object? sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded ||
+                e.ListChangedType == ListChangedType.ItemChanged ||
+                e.ListChangedType == ListChangedType.ItemDeleted)
+            {
+
+            }
+
+        }
+    }
+}
